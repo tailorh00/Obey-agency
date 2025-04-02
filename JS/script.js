99a687a9b944a56a134626773f97410d3287d018
henil tailor <tailorhenil0011@gmail.com>
2025-04-02 16:23:29 +0530

    page1 done

--- JS/script.js
+++ JS/script.js
@@ -50,12 +50,38 @@ function loaderAnime() {
 
   tl.from("#page1", {
     y: 1500,
-    duration: 0.5,
+    duration: 0.7,
     delay: 0.2,
   });
 
   tl.to("#loader", {
     display: "none",
   });
+
+  tl.from("#nav , .nav-part1 h5", {
+    opacity: 0,
+  });
+
+  tl.from(
+    ".hero h1,  .hero h2 , .hero h3",
+    {
+      y: 150,
+      duration: 0.5,
+      stagger: 0.05,
+    },
+    "-=0.7"
+  );
 }
+function cursurAnime() {
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to("#cursur", {
+      y: dets.y,
+      x: dets.x,
+    });
+  });
+
+  Shery.makeMagnet(".nav-part2 h5");
+}
+
 loaderAnime();
+cursurAnime();
